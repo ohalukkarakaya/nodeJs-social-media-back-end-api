557785fc58fea49f46384da91cc47808ef00b465
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-19 21:28:28 +0300

    build server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require("./routes/users");
+
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,
+  {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => {
+    console.log("MongoDB Status: Connected");
+  });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+});
