135e459ba025bce89669ba6f7933dd59b92e5286
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-20 01:18:24 +0300

    update-post

--- routes/post.js
+++ routes/post.js
@@ -13,7 +13,21 @@ router.post("/", async (req, res) => {
     }
 });
 
-//update post
+//Update post
+router.put("/:id", async (req, res) => {
+    try{
+      const post = await Post.findById(req.params.id);
+      if(post.userId === req.body.userId){
+        await post.updateOne({$set: req.body});
+        res.status(200).json("The post has been update");
+      }else{
+        res.status(403).json("you can only update your posts");
+      }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 //Delete post
 //Get a post
 //like post
