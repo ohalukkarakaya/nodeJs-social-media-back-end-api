8b839189828ae9fa92a1af20d75362e68702593f
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-20 00:54:52 +0300

    create-post

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
+const { response } = require("express");
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-    console.log("post route");
+//Create post
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try{
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    }catch(err){
+        response.status(500).json(err);
+    }
 });
+
+//update post
+//Delete post
+//Get a post
+//like post
+//Get timeline posts
 
 module.exports = router;
