9d38aeb5f18ed2bc5194513bcd1884ae5815b36f
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-20 01:41:06 +0300

    get-a-post

--- routes/post.js
+++ routes/post.js
@@ -44,6 +44,15 @@ router.delete("/:id", async (req, res) => {
 });
 
 //Get a post
+router.get("/:id", async (req,res) => {
+    try{
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    }catch(err){
+      res.status(500).json(err);
+    }
+});
+
 //Like - Dislike post
 router.put("/:id/like", async (req,res) => {
     try{
