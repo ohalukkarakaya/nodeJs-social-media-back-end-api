c771d0acf23cdb00c59fbcc45053ac55e14607d5
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-19 22:16:55 +0300

    post-user

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
-    usernamme: {
+    username: {
         type: String,
         required: true,
         min: 3,
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("auth route");
+//register
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    try{
+        const user = await newUser.save();
+        res.status(200).json(newUser);
+
+    }catch(err){
+        res.json(err.message);
+    }
 });
 
 module.exports = router
